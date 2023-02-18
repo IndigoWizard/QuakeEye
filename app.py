976f9e7e4033ff937632898420f108fff1799aaa
IndigoWizard <microclubist@gmail.com>
2023-02-18 23:47:38 +0100

    Setting up the Heat Map
    - adding secondary basemap "osm" (serves as light theme of a sort)
    - defining a color ramp for the heatmap
    - creating the heatmap

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import folium
+from folium.plugins import HeatMap
 import requests
 import webbrowser
 
@@ -31,7 +32,18 @@ m = folium.Map(location=[36.5, 37.5], tiles=None, zoom_start=3)
 
 #Primary basemaps
 basemap0 = folium.TileLayer("cartodbdark_matter", name="Dark Theme Basemap").add_to(m)
-basemap1 = folium.TileLayer("openstreetmap", name="Open Street Map").add_to(m)
+basemap1 = folium.TileLayer("openstreetmap", name="Open 6treet Map").add_to(m)
+
+# Defning a color ramp for the heat map
+colors = {0.2: '#0f0b75', 0.45: '#9e189c', 0.75: '#ed7c50', 1: '#f4ee27'}
+
+# Adding the folium heatmap layer using the HeatMap plugin
+heatmap = HeatMap(data=coords, gradient=colors, name="Earthquake distribution").add_to(m)
+
+
+
+# Adding the layer control
+folium.LayerControl(collapsed=False).add_to(m)
 
 m.save("map.html")
 webbrowser.open("map.html")
