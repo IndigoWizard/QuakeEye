d42268d5c83863d5a6b357d29a3c8dc4ca57f6d3
IndigoWizard <microclubist@gmail.com>
2023-02-28 23:46:06 +0100

    UI update 3
    - editing main page content + sidebar
    - creating appstyle.py to store project css
    - moving branca html/css to appstyle.py
    - moving streamlit app css to appstyle.py
    - adding gitignore file

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from folium.plugins import GroupedLayerControl
 from branca.element import Template, MacroElement
 import requests
 from datetime import datetime
-import webbrowser
+# my custom css
+import appstyle
 
 # Earthquake data GeoJSON URL:
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
@@ -42,7 +43,8 @@ st.sidebar.success("Real-Time Earthquake Data Visualization App.")
 
 # About section
 st.sidebar.subheader("About me:")
-st.sidebar.caption("Tech & Open Source enthusiast | Geo Environment & Spatial Planning | Remote Sensing & Cartography | Indie Games Dev | House of M.")
+st.sidebar.caption("**Ahmed I. Mokhtari (IndigoWizard):** <br> Tech & Open Source enthusiast | Geo Environment & Spatial Planning | Maps & Cartography | Remote Sensing & Geospatial Analysis | Indie Game Dev | House of M.",
+    unsafe_allow_html=True)
 
 # Contact section
 st.sidebar.subheader("Find me at:")
@@ -59,24 +61,7 @@ with c3:
     st.info("[![Medium](https://miro.medium.com/1*m-R_BkNf1Qjr1YbyOIJY2w.png)](https://medium.com/@Indigo.Wizard/mt-chenoua-forest-fires-analysis-with-remote-sensing-614681f468e9)")
 
 # App custom CSS
-st.markdown(
-    """
-    <style>
-    /*Main app page*/
-    .css-k1ih3n{
-        padding: 2rem 1rem 5rem;
-    }
-    /*Sidebar*/
-    .css-1vencpc {
-        width: 20vw !important;
-    }
-    .css-1vq4p4l{
-        padding: 1rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+st.markdown(appstyle.st_css,unsafe_allow_html=True,)
 
 ########### MAIN PAGE CONTENT ###########
 # App title
@@ -87,8 +72,8 @@ st.write("ʕ •ᴥ•ʔ : Dont forget to star ⭐ this project on [GitHub.com/I
 st.header("Earthquake Data Visualization")
 
 # Add a description of your Streamlit app
-st.write("This app visualizes the latest earthquake data from the USGS in real-time. The app retrieves earthquake data from the USGS API and displays the data on a map using the Folium library.")
-st.write("Users can filter and explore earthquake data by magnitude, location, and time range.")
+st.write("This app visualizes the latest earthquake data from [USGS](https://www.usgs.gov/) in real-time. The app retrieves earthquake data from the USGS API and displays the data on a map using the [Folium](https://python-visualization.github.io/folium/) library and is deployed using [Streamlit](https://streamlit.io/).")
+st.write("Users can filter and explore earthquake data by magnitude, location and frequency magnitude distribution.")
 
 st.subheader("Earthquake Map")
 
@@ -130,109 +115,7 @@ major_layer = folium.FeatureGroup(name="Major: 7.0 - 7.9").add_to(main_layer)
 great_layer = folium.FeatureGroup(name="Great: 8.0 and higher").add_to(main_layer)
 
 # Injecting custom css through branca macro elements and template
-app_css = """
-{% macro html(this, kwargs) %}
-    <head>
-        <title>QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari</title>
-        
-        <!-- Global Meta Tags -->
-        <meta itemprop="image" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif">
-        <meta itemprop="thumbnailUrl" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif">
-        <link rel="image_src" href="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif">
-        <link rel="shortcut icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/2377/2377860.png">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-        <link rel="stylesheet" href="style.css">
-
-        <!-- LinkediI Meta Tags -->
-        <meta property="og:url" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif"/>
-        <meta property="og:title" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari"/>
-        <meta property="og:description" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari"/>
-        <meta property="og:image" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif"/>
-
-        <!-- Facebook Meta Tags -->
-        <meta property="og:url" content="https://indigowizard.github.io/QuakeEye/map.html">
-        <meta property="og:type" content="website">
-        <meta property="og:title" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari">
-        <meta property="og:description" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari">
-        <meta property="og:image" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif">
-
-        <!-- Twitter Meta Tags -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta property="twitter:domain" content="https://indigowizard.github.io/QuakeEye/map.html">
-        <meta property="twitter:url" content="https://indigowizard.github.io/QuakeEye/map.html">
-        <meta name="twitter:title" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari">
-        <meta name="twitter:description" content="QuakeEye - Real-Time Earthquake Data Visualization by Ahmed I. Mokhtari">
-        <meta name="twitter:image" content="https://user-images.githubusercontent.com/43890965/221388610-ab938380-7c0f-46bc-be71-6ee2031cb6bb.gif">
-
-        <!-- Meta Tags Generated via https://www.opengraph.xyz -->
-    </head>
-    
-    <style>
-/* Marker PopUp Box CSS */
-        .leaflet-popup-content-wrapper{
-            padding: 1px;
-            text-align: left;
-            border: 4px solid #d7a45d;
-            border-radius: 12px;
-            /*GLASSMORPHISM EFFECT*/
-            background: rgba( 28, 25, 56, 0.25 );
-            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-            backdrop-filter: blur( 4px );
-            -webkit-backdrop-filter: blur( 4px );
-            border: 4px solid rgba( 215, 164, 93, 0.2 );
-        }
-        .leaflet-popup-content{
-            margin: 13px 24px 13px 20px;
-            font-size: 1.2em;
-            line-height: 1.3;
-            min-height: 1px;
-        }
-        .popinfo {
-            width: max-content;
-            border-radius: 5px;
-            color: #993200;
-        }
-        .popinfo h5{
-            text-align: center;
-        }
-        .popinfo span{
-            font-weight: bold;
-            color: #9d3a00;
-        }
-        .popinfo b{
-            font-weight: 800;
-            font-family: bariol;
-            font-size: 1.4em;
-        }
-        .leaflet-popup-tip{
-            background: #d7a45d !important;
-        }
-
-/* Layer Control Panel CSS */
-
-        .leaflet-control-layers-list {
-            width: 14vw;
-            min-width: 130px;
-            overflow-y: auto;
-            overflow-x: hidden;
-        }
-        .leaflet-control-layers form {
-            z-index: 10000;
-            overflow-y: auto;
-            overflow-x: hidden;
-        }
-        .leaflet-control-layers-group-label{
-            padding: 2px;
-            margin: 2px;
-            background-color: #d75d5d;
-            border: 1px dashed black;
-            border-radius: 4px;
-            text-align: center;
-        }
-    </style>
-
-{% endmacro %}
-"""
+app_css = appstyle.map_css
 # configuring the style
 style = MacroElement()
 style._template = Template(app_css)
