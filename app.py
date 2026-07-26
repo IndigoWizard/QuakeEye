657b804a5eb8d07bf8ab92866502fd2a1967dea4
IndigoWizard <microclubist@gmail.com>
2026-07-26 22:51:45 +0100

    prevent app rerun
    the app reruns when interacting with the folium map (panning, zooming, etc): had to use st.cache_data for the usgs api data, and return empty list for the returned objects in st_folium() which solved the issue.

--- app.py
+++ app.py
@@ -153,6 +153,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 DATA_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 
 # fetch earthquake GeoJSON data from USGS API
+@st.cache_data(persist=True)
 def fetch_earthquake_data():
     response = requests.get(DATA_URL)
     if response.status_code == 200:
@@ -421,7 +422,7 @@ def main():
             folium.LayerControl(collapsed=True).add_to(m)
 
             # Display the map
-            st_folium(m, width="stretch", height="600")
+            st_folium(m, width="stretch", height="600", returned_objects=[])
 
     st.divider()
 
